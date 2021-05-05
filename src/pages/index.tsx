e4e0cf197809ca036f0249655db29017a8f59337
Gabriel Maia <gabrielmaia@protonmail.com>
2021-05-05 18:13:42 +0100

    Created index page and header component

---
+++ src/pages/index.tsx
@@ -0,0 +1,28 @@
+import { Flex, Button, Stack } from "@chakra-ui/react";
+
+import { Input } from "../components/Form/Input";
+
+export default function SignIn() {
+  return (
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDirection="column"
+      >
+        <Stack spacing="4">
+          <Input name="email" type="email" label="E-mail" />
+          <Input name="password" type="password" label="Password" />
+        </Stack>
+
+        <Button type="submit" mt="6" colorScheme="pink" size="lg">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  );
+}
